49e36d7bef67a7b1baa8403467f29355e5a083e0
Jenkins User <jenkins_user@datical.com>
2020-03-31 11:37:09 -0500

    change script.

--- sql_code/1-ddl/create_table_table1.sql
+++ sql_code/1-ddl/create_table_table1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE APP_SCHEMA_1.TABLE_1 (
     PersonID int,
     LastName varchar(256),
-    FirstName varchar(255),
+    FirstName varchar(256),
     Address varchar(255),
     City varchar(255)
 );
